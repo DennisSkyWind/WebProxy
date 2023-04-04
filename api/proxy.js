6c2b1400bdb6ab2912029ff18b857226ef0ee35a
DennisSkyWind <128893759+DennisSkyWind@users.noreply.github.com>
2023-04-04 09:49:32 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,11 +1,24 @@
 const httpProxy = require('http-proxy');
-
 const proxy = httpProxy.createProxyServer({
   target: 'https://chat.openai.com',
   changeOrigin: true,
-  secure: false
+  secure: false,
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+    'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization'
+  }
 });
 
 export default function(req, res) {
-  proxy.web(req, res);
+  if (req.method === 'OPTIONS') {
+    res.writeHead(200, {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization'
+    });
+    res.end();
+  } else {
+    proxy.web(req, res);
+  }
 }
